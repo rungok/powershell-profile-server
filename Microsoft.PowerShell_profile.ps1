b1c1d49e30ad4d6d08d325c4b5358efa15d73a7d
Rune Goksør <57212482+rungok@users.noreply.github.com>
2024-09-09 11:36:21 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,23 @@
-### PowerShell Profile Refactor
-### Version 1.03 - Refactored
+#############################################################################################
+$tit = 'PowerShell-Profile-Server Pimp v1.7 by RUNE GOKS0R'	 		  	    #
+$githubUser = 'rungok'									    #
+$PoshTheme = 'markbull'  # Write Get-PoshThemes to see all themes in action                 #
+#  Content of this script should be put in this file (just enter that command in PS)        #
+#  Notepad $PROFILE                                                                         #
+#  After pasting in this script in that file, add # to the line above.			    #
+#  Recommended requirement: Windows Terminal						    #
+#		https://github.com/microsoft/terminal/releases)   			    #
+#############################################################################################
 
-#################################################################################################################################
-############                                                                                                         ############
-############                                          !!!   WARNING:   !!!                                           ############
-############                                                                                                         ############
-############                DO NOT MODIFY THIS FILE. THIS FILE IS HASHED AND UPDATED AUTOMATICALLY.                  ############
-############                    ANY CHANGES MADE TO THIS FILE WILL BE OVERWRITTEN BY COMMITS TO                      ############
-############                       https://github.com/ChrisTitusTech/powershell-profile.git.                         ############
-############                                                                                                         ############
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
-############                                                                                                         ############
-############                      IF YOU WANT TO MAKE CHANGES, USE THE Edit-Profile FUNCTION                         ############
-############                              AND SAVE YOUR CHANGES IN THE FILE CREATED.                                 ############
-############                                                                                                         ############
-#################################################################################################################################
+Write-Host("`n         .--------< ") -f white -nonewline
+Write-Host($tit) -f Cyan -nonewline
+Write-Host(" >---------------------.") -f white
+Write-Host("         '-----------------------------------------------------------------------------------'`n") -f white
+
+$execPolicy = Get-ExecutionPolicy
+if ($execPolicy -ne "RemoteSigned") {
+        Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
+}
 
 #opt-out of telemetry before doing anything, only if PowerShell is run as admin
 if ([bool]([System.Security.Principal.WindowsIdentity]::GetCurrent()).IsSystem) {
@@ -24,15 +27,62 @@ if ([bool]([System.Security.Principal.WindowsIdentity]::GetCurrent()).IsSystem) 
 # Initial GitHub.com connectivity check with 1 second timeout
 $canConnectToGitHub = Test-Connection github.com -Count 1 -Quiet -TimeoutSeconds 1
 
+# Install NuGet to ensure the other packages can be installed.
+$nugetProvider = Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue
+if (-not $nugetProvider) {
+    Write-Host "NuGet provider not found. Installing..."
+    Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
+    Import-PackageProvider -Name NuGet -Force
+    Write-Host "NuGet provider installed."
+} else {
+    Write-Host "✅ NuGet provider detected." -ForegroundColor DarkGreen
+}
+# Trust the PSGallery repository.
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+# Install opensource Powershell
+function Update-PowerShell {
+	if (-not (Get-Command pwsh -ErrorAction SilentlyContinue)) {
+		Write-Host "PowerShell Core (pwsh) is not installed. Starting the update..." -ForegroundColor Yellow
+		Run-UpdatePowershell
+		Start-Sleep -Seconds 8 # Wait for the update to finish
+		Write-Host "Restarting the installation script with Powershell Core" -ForegroundColor DarkGreen
+		Start-Process pwsh -ArgumentList "-NoExit", "-Command Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/$githubUser/powershell-profile-server/main/Microsoft.PowerShell_profile.ps1'-UseBasicParsing).Content ; Install-Config"
+		exit
+		} else { Write-Host "✅ PowerShell Core (pwsh) detected." -ForegroundColor DarkGreen }
+}
+Update-PowerShell
+
+### Install NerdFont (font with CLI icons for a bunch of stuff)
+If (-not(Test-Path "$($env:LOCALAPPDATA)\Microsoft\Windows\Fonts\RobotoMonoNerdFontMono-Regular.ttf")) {
+	Write-Host ("Does not exist. Trying to install...") -nonewline -f red
+    & ([scriptblock]::Create((iwr 'https://to.loredo.me/Install-NerdFont.ps1'))) -Confirm:$false -Name roboto-mono
+	Write-Host ("installed!") -f green	
+	Write-Host ("There is no command that can change the font for you in Powershell. Change to RobotoMono in Terminal settings.") -f green
+	} Else { Write-Host "✅ RobotoMono Nerd Font detected." -f DarkGreen }
+
+
 # Import Modules and External Profiles
 # Ensure Terminal-Icons module is installed before importing
 if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
     Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck
 }
 Import-Module -Name Terminal-Icons
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-    Import-Module "$ChocolateyProfile"
+
+
+function Test-CreateProfile {
+    # Create $PATH folder if not exists.
+    if (-not (Test-Path -Path (Split-Path -Path $PROFILE -Parent))) {
+        New-Item -ItemType Directory -Path (Split-Path -Path $PROFILE -Parent) -Force | Out-Null
+    }
+    # Create profile if not exists
+    if (-not (Test-Path -Path $PROFILE)) {
+        New-Item -ItemType File -Path $PROFILE | Out-Null
+        Add-Content -Path $PROFILE -Value "if (Test-Path (Join-Path -Path `$env:USERPROFILE -ChildPath `"powershell-profile-server\Microsoft.PowerShell_profile.ps1`")) { . (Join-Path -Path `$env:USERPROFILE -ChildPath `"powershell-profile-server\Microsoft.PowerShell_profile.ps1`")
+	} else {
+		iex (iwr `"https://raw.githubusercontent.com/$githubUser/powershell-profile-server/main/Microsoft.PowerShell_profile.ps1`").Content }"
+        Write-Host "PowerShell profile created at $PROFILE." -ForegroundColor Yellow
+    }
 }
 
 # Check for Profile Updates
@@ -43,7 +93,7 @@ function Update-Profile {
     }
 
     try {
-        $url = "https://raw.githubusercontent.com/ChrisTitusTech/powershell-profile/main/Microsoft.PowerShell_profile.ps1"
+        $url = "https://raw.githubusercontent.com/$githubUser/powershell-profile-server/main/Microsoft.PowerShell_profile.ps1"
         $oldhash = Get-FileHash $PROFILE
         Invoke-RestMethod $url -OutFile "$env:temp/Microsoft.PowerShell_profile.ps1"
         $newhash = Get-FileHash "$env:temp/Microsoft.PowerShell_profile.ps1"
@@ -57,39 +107,19 @@ function Update-Profile {
         Remove-Item "$env:temp/Microsoft.PowerShell_profile.ps1" -ErrorAction SilentlyContinue
     }
 }
-Update-Profile
 
-function Update-PowerShell {
-    if (-not $global:canConnectToGitHub) {
-        Write-Host "Skipping PowerShell update check due to GitHub.com not responding within 1 second." -ForegroundColor Yellow
-        return
-    }
+######################################################################
+##### Setting aliases spesific to PowerShell-Profile-Server Pimp #####
+######################################################################
+New-Alias Notepad "$env:Programfiles\Notepad++\Notepad++.exe"
+New-Alias np "$env:Programfiles\Notepad++\Notepad++.exe"
+New-Alias vi "$env:Programfiles\Notepad++\Notepad++.exe"
+function hf {Get-Content (Get-PSReadlineOption).HistorySavePath}
+New-Alias Get-FullHistory hf
+function Path { $env:Path }
+function PathX { $env:Path -split ';' }
 
-    try {
-        Write-Host "Checking for PowerShell updates..." -ForegroundColor Cyan
-        $updateNeeded = $false
-        $currentVersion = $PSVersionTable.PSVersion.ToString()
-        $gitHubApiUrl = "https://api.github.com/repos/PowerShell/PowerShell/releases/latest"
-        $latestReleaseInfo = Invoke-RestMethod -Uri $gitHubApiUrl
-        $latestVersion = $latestReleaseInfo.tag_name.Trim('v')
-        if ($currentVersion -lt $latestVersion) {
-            $updateNeeded = $true
-        }
-
-        if ($updateNeeded) {
-            Write-Host "Updating PowerShell..." -ForegroundColor Yellow
-            winget upgrade "Microsoft.PowerShell" --accept-source-agreements --accept-package-agreements
-            Write-Host "PowerShell has been updated. Please restart your shell to reflect changes" -ForegroundColor Magenta
-        } else {
-            Write-Host "Your PowerShell is up to date." -ForegroundColor Green
-        }
-    } catch {
-        Write-Error "Failed to update PowerShell. Error: $_"
-    }
-}
-Update-PowerShell
-
-
+##### Aliases and functions spesific to forked powershell-profile ##### 
 # Admin Check and Prompt Customization
 $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 function prompt {
@@ -189,6 +219,7 @@ function hb {
         Write-Error "Failed to upload the document. Error: $_"
     }
 }
+
 function grep($regex, $dir) {
     if ( $dir ) {
         Get-ChildItem $dir | select-string $regex
@@ -329,113 +360,113 @@ function Get-Theme {
             return
         }
     } else {
-        oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/cobalt2.omp.json | Invoke-Expression
+        oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/$PoshTheme.omp.json | Invoke-Expression
     }
 }
-
 ## Final Line to set prompt
 Get-Theme
-if (Get-Command zoxide -ErrorAction SilentlyContinue) {
-    Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
-} else {
-    Write-Host "zoxide command not found. Attempting to install via winget..."
-    try {
-        winget install -e --id ajeetdsouza.zoxide
-        Write-Host "zoxide installed successfully. Initializing..."
-        Invoke-Expression (& { (zoxide init powershell | Out-String) })
-    } catch {
-        Write-Error "Failed to install zoxide. Error: $_"
-    }
+
+### Install Chocolatey if not installed and shell is started in administrative mode ####
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+	Write-Host ("❌ Chocolatey packet manager not installed...") -nonewline -f red
+	if ($isAdmin) {
+		Write-Host ("Trying to install...") -nonewline -f DarkGreen
+		Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+		} else { Write-Host ("❌ Terminal must be started in elevated mode to install Chocolatey. Zoxide fuzzy shell will not be activated until this is done.") -f red }
+	} else {
+		Write-Host "✅ Chocolatey packet manager detected." -f DarkGreen
+		$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+		if (Test-Path($ChocolateyProfile)) {
+			Import-Module "$ChocolateyProfile"
+		}
 }
 
-Set-Alias -Name z -Value __zoxide_z -Option AllScope -Scope Global -Force
-Set-Alias -Name zi -Value __zoxide_zi -Option AllScope -Scope Global -Force
+### Install zoxide fuzzy shell if not installed and shell is started in administrative mode ####
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+	Write-Host "✅ Zoxide detected." -ForegroundColor DarkGreen
+	Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+	Set-Alias -Name z -Value __zoxide_z -Option AllScope -Scope Global -Force
+	Set-Alias -Name zi -Value __zoxide_zi -Option AllScope -Scope Global -Force
+} else {
+	if ($isAdmin) {
+		Write-Host "❌ Zoxide command not found. Attempting to install via Chocolatey..." -nonewline -f red
+		try {
+			choco install zoxide -y
+			Invoke-Expression (& { (zoxide init powershell | Out-String) })
+			Write-Host "✅ Zoxide installed successfully. Initializing..." -ForegroundColor DarkGreen
+		} catch {
+			Write-Error "❌ Failed to install zoxide. Error: $_"
+		}
+	} else { Write-Host ("❌ Terminal must be started in elevated mode to install Zoxide. Fuzzy shell will not be activated until this is done.") -f red }
+}
 
 # Help Function
 function Show-Help {
     @"
-PowerShell Profile Help
-=======================
+Help for $tit
+.======================================================================.
+| Path - Prints out current users Path like MS-DOS.                    |
+| PathX - Prints out current users Path like MS-DOS in listed format.  |
+| np - Notepad++ full qualified path, so use np <file.txt> to edit.    |
+| vi - same as np                                                      |
+| hf - Full commandline history (also Get-FullHistory works)           |
+'----------------------------------------------------------------------'
 
+---------------------- original commands from forked "powershell-profile" ---------------------------------------------
 Update-Profile - Checks for profile updates from a remote repository and updates if necessary.
-
 Update-PowerShell - Checks for the latest PowerShell release and updates if a new version is available.
-
 Edit-Profile - Opens the current user's profile for editing using the configured editor.
-
 touch <file> - Creates a new empty file.
-
 ff <name> - Finds files recursively with the specified name.
-
 Get-PubIP - Retrieves the public IP address of the machine.
-
 winutil - Runs the WinUtil script from Chris Titus Tech.
-
 uptime - Displays the system uptime.
-
 reload-profile - Reloads the current user's PowerShell profile.
-
 unzip <file> - Extracts a zip file to the current directory.
-
 hb <file> - Uploads the specified file's content to a hastebin-like service and returns the URL.
-
 grep <regex> [dir] - Searches for a regex pattern in files within the specified directory or from the pipeline input.
-
 df - Displays information about volumes.
-
 sed <file> <find> <replace> - Replaces text in a file.
-
 which <name> - Shows the path of the command.
-
 export <name> <value> - Sets an environment variable.
-
 pkill <name> - Kills processes by name.
-
 pgrep <name> - Lists processes by name.
-
 head <path> [n] - Displays the first n lines of a file (default 10).
-
 tail <path> [n] - Displays the last n lines of a file (default 10).
-
 nf <name> - Creates a new file with the specified name.
-
 mkcd <dir> - Creates and changes to a new directory.
-
 docs - Changes the current directory to the user's Documents folder.
-
 dtop - Changes the current directory to the user's Desktop folder.
-
 ep - Opens the profile for editing.
-
 k9 <name> - Kills a process by name.
-
 la - Lists all files in the current directory with detailed formatting.
-
 ll - Lists all files, including hidden, in the current directory with detailed formatting.
-
 gs - Shortcut for 'git status'.
-
 ga - Shortcut for 'git add .'.
-
 gc <message> - Shortcut for 'git commit -m'.
-
 gp - Shortcut for 'git push'.
-
 g - Changes to the GitHub directory.
-
 gcom <message> - Adds all changes and commits with the specified message.
-
 lazyg <message> - Adds all changes, commits with the specified message, and pushes to the remote repository.
-
 sysinfo - Displays detailed system information.
-
 flushdns - Clears the DNS cache.
-
 cpy <text> - Copies the specified text to the clipboard.
-
 pst - Retrieves text from the clipboard.
-
+z - ehanced zoxide CD (change directory) that guess which directory you want to change to based on history.
+Get-PoshThemes - See overview of all Oh-My-Posh themes in action based on your current folder
+-------------------------------------------------------------------------------------------------------------------------
 Use 'Show-Help' to display this help message.
 "@
 }
-Write-Host "Use 'Show-Help' to display help"
+
+Write-Host "Write 'Show-Help' to display overview of enhanced PowerShell commands in this setup"
+
+# Install and execute WinFetch (neofetch-port to powershell) - requires PSGallery
+if (-not(Get-InstalledScript -Name winfetch)) { Install-Script winfetch -Force }
+	# Get full path to this scripts path to execute winfetch from System Powershell folder + winfetch:
+	# $FetchImage = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "winfetch\windows-7-logo2.png"))
+	# $FetchConfig = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "winfetch\config.ps1"))
+	# $FetchScript = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "Scripts\winfetch.ps1"))
+	# Invoke-expression -Command "$FetchScript -configpath $FetchConfig -Image $FetchImage"
+winfetch
+
